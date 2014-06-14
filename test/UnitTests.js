f7a1135552496f7bf784eeee8f3fa8dc1d9de4bf
Brett Ausmeier <brett.ausmeier@bsg.co.za>
2014-06-14 16:14:34 +0200

    Update api url

--- test/UnitTests.js
+++ test/UnitTests.js
@@ -9,24 +9,24 @@ var BitX = require('../lib/BitX'),
 var expect = chai.expect;
 
 describe('Constructor', function() {
-  
+
   it('should create a new instance', function() {
     var bitx = new BitX();
     expect(bitx).to.be.an.instanceOf(BitX);
   });
-  
+
   it('should create a new instance without the new keyword', function() {
     var bitx = BitX();
     expect(bitx).to.be.an.instanceOf(BitX);
   });
-  
+
   it('should have default options', function() {
     var bitx = new BitX();
-    expect(bitx.hostname).to.equal('bitx.co.za');
+    expect(bitx.hostname).to.equal('api.mybitx.com');
     expect(bitx.port).to.equal(443);
     expect(bitx.pair).to.equal('XBTZAR');
   });
-  
+
   it('should accept options', function() {
     var options = {
       hostname: 'localhost',
@@ -38,7 +38,7 @@ describe('Constructor', function() {
     expect(bitx.port).to.equal(options.port);
     expect(bitx.pair).to.equal(options.pair);
   });
-  
+
   it('should accept auth and options', function() {
     var keyId = 'cnz2yjswbv3jd',
         keySecret = '0hydMZDb9HRR3Qq-iqALwZtXLkbLR4fWxtDZvkB9h4I',
@@ -56,7 +56,7 @@ describe('Constructor', function() {
 });
 
 describe('Internal', function() {
-  
+
   var bitx,
       request,
       stub;
@@ -64,34 +64,34 @@ describe('Internal', function() {
   var keyId = '12345',
       keySecret = '0000000000000000',
       path = 'test';
-  
+
   var expectedOptions = {
     headers: {
       'Accept': 'application/json',
       'Accept-Charset': 'utf-8'
     },
-    hostname: 'bitx.co.za',
+    hostname: 'api.mybitx.com',
     path: '/api/1/' + path,
     port: 443,
     auth: keyId + ':' + keySecret
   };
-  
+
   before(function() {
     bitx = new BitX(keyId, keySecret);
     request = sinon.stub(https, 'request'),
     stub = request.withArgs(sinon.match(expectedOptions));
   });
-  
+
   after(function() {
     request.restore();
   });
-  
+
   afterEach(function() {
     stub.reset();
   });
-  
+
   describe('_request', function() {
-    
+
     it('should return the expected result', function(done) {
       var expectedResult = {success: true};
       stub.returns(new FakeRequest(expectedResult));
@@ -101,7 +101,7 @@ describe('Internal', function() {
         done();
       });
     });
-    
+
     it('should return an error if request emits an error', function(done) {
       stub.returns(new FakeRequest(null, {fail: true}));
       bitx._request('GET', path, null, function(err, result) {
@@ -109,7 +109,7 @@ describe('Internal', function() {
         done();
       });
     });
-    
+
     it('should return an error if the response is not valid', function(done) {
       stub.returns(new FakeRequest('invalid'));
       bitx._request('GET', path, null, function(err, result) {
@@ -117,7 +117,7 @@ describe('Internal', function() {
         done();
       });
     });
-    
+
     it('should return an error if the response contains an error', function(done) {
       stub.returns(new FakeRequest({error: true}));
       bitx._request('GET', path, null, function(err, result) {
@@ -125,7 +125,7 @@ describe('Internal', function() {
         done();
       });
     });
-    
+
     it('should return an error if the response is unauthorized', function(done) {
       stub.returns(new FakeRequest(null, {statusCode: 401}));
       bitx._request('GET', path, null, function(err, result) {
@@ -139,19 +139,19 @@ describe('Internal', function() {
 describe('External', function() {
   var bitx,
       mock;
-  
+
   var callback = function() {};
-  
+
   before(function() {
     bitx = new BitX();
   });
-  
+
   beforeEach(function() {
     mock = sinon.mock(bitx);
   });
-  
+
   describe('getTicker', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       mock.expects('_request').once().withArgs(
         'GET', 'ticker', {pair: 'XBTZAR'}, callback);
@@ -159,9 +159,9 @@ describe('External', function() {
       mock.verify();
     });
   });
-  
+
   describe('getOrderBook', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       mock.expects('_request').once().withArgs(
         'GET', 'orderbook', {pair: 'XBTZAR'}, callback);
@@ -169,9 +169,9 @@ describe('External', function() {
       mock.verify();
     });
   });
-  
+
   describe('getTrades', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       mock.expects('_request').once().withArgs(
         'GET', 'trades', {pair: 'XBTZAR'}, callback);
@@ -179,9 +179,9 @@ describe('External', function() {
       mock.verify();
     });
   });
-  
+
   describe('getOrderList', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       mock.expects('_request').once().withArgs(
         'GET', 'listorders', {pair: 'XBTZAR'}, callback);
@@ -189,9 +189,9 @@ describe('External', function() {
       mock.verify();
     });
   });
-  
+
   describe('getLimits', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       mock.expects('_request').once().withArgs(
         'GET', 'BTCZAR/getlimits', null, callback);
@@ -201,7 +201,7 @@ describe('External', function() {
   });
 
   describe('postBuyOrder', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       var parameters = {
         type: 'BID',
@@ -214,9 +214,9 @@ describe('External', function() {
       mock.verify();
     });
   });
-  
+
   describe('postSellOrder', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       var parameters = {
         type: 'ASK',
@@ -229,9 +229,9 @@ describe('External', function() {
       mock.verify();
     });
   });
-  
+
   describe('stopOrder', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       var body = {
         order_id: 'BXMC2CJ7HNB88U4'
@@ -243,7 +243,7 @@ describe('External', function() {
   });
 
   describe('getBalance', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       mock.expects('_request').once().withArgs(
         'GET', 'balance', {asset: 'ZAR'}, callback);
@@ -253,7 +253,7 @@ describe('External', function() {
   });
 
   describe('getFundingAddress', function() {
-    
+
     it('should call _request with the correct parameters', function() {
       mock.expects('_request').once().withArgs(
         'GET', 'funding_address', {asset: 'XBT'}, callback);
